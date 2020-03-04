37b94ddfec6e1cb1f7d4cc24398660c577d029c8
Ivan Alexandrov <ivan.alexandrov@syntacore.com>
2020-03-04 12:16:02 +0300

    prime func

--- vars/log.groovy
+++ vars/log.groovy
@@ -1,3 +1,11 @@
+@Grab('org.apache.commons:commons-math3:3.4.1')
+import org.apache.commons.math3.primes.Primes
+void parallelize(int count) {
+  if (!Primes.isPrime(count)) {
+    error "${count} was not prime"
+  }
+}
+
 def helloMe(message) {
     echo "HELLO WORLD ${message}"
 }
