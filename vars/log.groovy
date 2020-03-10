0cd6dcb9ddf0d5ed8862e826ed47355673278717
Ivan Alexandrov <ivan.alexandrov@syntacore.com>
2020-03-10 11:48:55 +0300

    new one

--- vars/log.groovy
+++ vars/log.groovy
@@ -56,10 +56,10 @@ def prepareBuildDir(){
   for (f in findFiles(glob: "**/regression_default_*.yaml")){
     echo "${f.path}"
     String scenario = "${f.path}".split("/")[-1]
-    String tests = "${f.path}".split("/")[-2]
+    String tests = "${f.path}".split("/")[-3]
     String folder = "${f.path}".split("/")[1]
     String build_dir = "build/"+"${f.path}".split("/")[1]
-    String launcherPath = "${f.path}".split(tests)[0]
+    String launcherPath = "${f.path}".split(tests)[0] + "/framework/launcher/launch.pl"
     // rename
     echo scenario
     echo launcherPath
