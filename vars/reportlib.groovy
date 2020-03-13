2a95d46bd9705b0f3915e34c7d3c9bc275168823
Ivan Alexandrov <ivan.alexandrov@syntacore.com>
2020-03-13 13:32:19 +0300

    path changed

--- vars/reportlib.groovy
+++ vars/reportlib.groovy
@@ -172,9 +172,12 @@ def generateHTMLreport(build_dir){
             )
         )
         markup.table(style: 'border:2px solid;padding: 2px;text-align:center;style: "border-collapse:collapse;"'){
-            markup.tr(
-                markup.td(class:"row", "hello world!")  
-            )
+            markup.thead()
+                markup.tr(
+                    markup.th(title:"Field #1", class:"header", "Config name")
+                    markup.th(title:"Field #2", class:"header", "Failed tests")
+                ) // tr
+            ) // thead
         }
     }
     resultString = resultString.concat(writer.toString())
