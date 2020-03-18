2d73230f039ae331de4b1d72ff05c93092a2a013
Ivan Alexandrov <ivan.alexandrov@syntacore.com>
2020-03-18 16:17:02 +0300

    pushToVm added

--- vars/pushToVm.groovy
+++ vars/pushToVm.groovy
@@ -1,33 +1,31 @@
 def call(){
     stage('Push Project to agent VM') {
-        steps {
-            echo "======== PUSH to 192.168.1.110 ========"
-            sshPublisher(
-                publishers: [
-                    sshPublisherDesc(
-                    configName: 'SSHPUB110',
-                    transfers: [
-                        sshTransfer(
-                            cleanRemote: true,
-                            excludes: '',
-                            execCommand: '',
-                            execTimeout: 120000,
-                            flatten: false,
-                            makeEmptyDirs: false,
-                            noDefaultExcludes: false,
-                            patternSeparator: '[, ]+',
-                            remoteDirectory: "${JOB_NAME}",
-                            remoteDirectorySDF: false,
-                            removePrefix: '',
-                            sourceFiles: '**'
-                        )
-                    ],
-                    usePromotionTimestamp: false,
-                    useWorkspaceInPromotion: false,
-                    verbose: false
+        echo "======== PUSH to 192.168.1.110 ========"
+        sshPublisher(
+            publishers: [
+                sshPublisherDesc(
+                configName: 'SSHPUB110',
+                transfers: [
+                    sshTransfer(
+                        cleanRemote: true,
+                        excludes: '',
+                        execCommand: '',
+                        execTimeout: 120000,
+                        flatten: false,
+                        makeEmptyDirs: false,
+                        noDefaultExcludes: false,
+                        patternSeparator: '[, ]+',
+                        remoteDirectory: "${JOB_NAME}",
+                        remoteDirectorySDF: false,
+                        removePrefix: '',
+                        sourceFiles: '**'
                     )
-                ]
-            )
-        }
+                ],
+                usePromotionTimestamp: false,
+                useWorkspaceInPromotion: false,
+                verbose: false
+                )
+            ]
+        )    
     }
 }
