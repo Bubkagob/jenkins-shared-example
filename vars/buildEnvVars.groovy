4344c12ed20a74ea61d0b8d0d284b8a09480b116
Ivan Alexandrov <ivan.alexandrov@syntacore.com>
2020-03-18 14:01:35 +0300

    new wave

--- vars/buildEnvVars.groovy
+++ vars/buildEnvVars.groovy
@@ -1,6 +1,4 @@
-@NonCPS
 def call() {
-    BUILD_USER = currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
     JOB_NAME = currentBuild.fullDisplayName
     //env.FTP_DIR = new Date().format("yy_MM_dd_${BUILD_NUMBER}", TimeZone.getTimeZone('Europe/Moscow'))
 }
