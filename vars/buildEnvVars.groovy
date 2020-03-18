488816fe064c78d54a43e6deef9c21259f638b1c
Ivan Alexandrov <ivan.alexandrov@syntacore.com>
2020-03-18 14:15:02 +0300

    scm added

--- vars/buildEnvVars.groovy
+++ vars/buildEnvVars.groovy
@@ -1,5 +1,4 @@
 def call() {
     JOB_NAME = currentBuild.fullDisplayName
-    //env.FTP_DIR = new Date().format("yy_MM_dd_${BUILD_NUMBER}", TimeZone.getTimeZone('Europe/Moscow'))
 }
 
--- vars/user.groovy
+++
@@ -1,3 +0,0 @@
-def BUILD_USER = currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
-def jobName = currentBuild.fullDisplayName
-FTP_DIR = new Date().format("yy_MM_dd_${BUILD_NUMBER}", TimeZone.getTimeZone('Europe/Moscow'))
