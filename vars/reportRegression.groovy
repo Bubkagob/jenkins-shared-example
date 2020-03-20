ff82d553770e0ce196664c02788c7f5cf375fa5d
Ivan Alexandrov <ivan.alexandrov@syntacore.com>
2020-03-20 11:35:30 +0300

    reportRegression added

---
+++ vars/reportRegression.groovy
@@ -0,0 +1,102 @@
+#!/usr/bin/env groovy
+def call(currentBuild) {
+    def choices = scenarios.join(',')
+    pipeline {
+        agent {
+            label "beta"
+        }
+        environment {
+            BUILD_USER = currentBuild.rawBuild.getCause(Cause.UserIdCause).getUserId()
+            FTP_DIR = new Date().format("yy_MM_dd_${BUILD_NUMBER}", TimeZone.getTimeZone('Europe/Moscow'))
+        }
+        options {
+            buildDiscarder(logRotator(numToKeepStr:'5'))
+            disableConcurrentBuilds()
+            timestamps()
+        }
+        stages{
+            stage("Analyze and Collect antifacts"){
+                agent{
+                    label "power"
+                }
+                steps{
+                    echo "======== Generate failed.txt ========"
+                    script {
+                        if (fileExists('artifacts.zip')) {
+                            sh "rm artifacts.zip"
+                        }
+                    }
+                    zip(
+                        archive: true,
+                        zipFile: 'artifacts.zip',
+                        dir: 'build',
+                        glob: '**/*results.txt, *failed.txt, **/*.xlsx, **/*.json, **/build.log'
+                    )
+                }
+            }
+            stage("Download artifacts from 110VM"){
+                steps{
+                    echo "========build & run drhtystone test========"
+                    sh "scp -o StrictHostKeyChecking=no -P 64013 physdesign@192.168.1.110:/home/work/jenkins/workspace/${JOB_NAME}/artifacts.zip artifacts.zip"
+                    sh 'ls -la'
+                    unzip(
+                        zipFile: 'artifacts.zip',
+                        dir: 'build'
+                    )
+                    sh 'ls -la'
+                }
+            }
+            stage("create reports"){
+                steps{
+                    echo "======== Generate reports with groovy scripts ========"
+                    script {
+                        def ts = new Date()
+                        def resultObject = reportlib.generateTextReport("${WORKSPACE}/build")
+                        env.GIT_BRANCH = scmVars.GIT_BRANCH
+                        env.BUILD_URL = "${BUILD_URL}"
+                        env.GIT_COMMIT = scmVars.GIT_COMMIT
+                        env.START_TIME = "${BUILD_TIMESTAMP}"
+                        env.BUILD_DATE = ts.format("yyyy-MM-dd", TimeZone.getTimeZone('Europe/Moscow'))
+                        env.COMPLETE_TIME = ts.format("EEE, MMMM dd, yyyy, HH:mm:ss '('zzz')'", TimeZone.getTimeZone('Europe/Moscow'))
+                        env.REPORT = resultObject["report"]
+                        env.TOTAL_TESTS = resultObject["total"]
+                        env.FAILED_TESTS = resultObject["failed"]
+                        writeFile(
+                            file: "report.txt",
+                            text: "${REPORT}" 
+                        )
+                        def HTML_REPORT = reportlib.generateHTMLreport("${WORKSPACE}/build")
+                        writeFile(
+                            file: "report.html",
+                            text: "${HTML_REPORT}" 
+                        )
+                        archiveArtifacts(
+                            artifacts: 'artifacts.zip, report.txt, report.html',
+                            fingerprint: true
+                        )
+                    }
+                }
+            }
+            stage("Publish html"){
+                steps{
+                    echo "======== Publish HTML report ========"
+                    script {
+                        if (fileExists('report.html')) {
+                            publishHTML(
+                                target : [
+                                    allowMissing: false,
+                                    alwaysLinkToLastBuild: true,
+                                    keepAll: true,
+                                    reportDir: '',
+                                    reportFiles: 'report.html',
+                                    reportName: 'Regression Report',
+                                    reportTitles: 'The Report'
+                                ]
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
