99b09321d78e91d8f84f0be11f769a39437ca479
Ivan Alexandrov <i1729van@gmail.com>
2020-03-23 14:02:20 +0300

    added env.scm

--- vars/rtpublish.groovy
+++ vars/rtpublish.groovy
@@ -1,12 +1,15 @@
 def call(){
     currentBuild.result = "SUCCESS"
     String result = currentBuild.result?:"SUCCESS"
-    String scmVars = env.scmVars?: "no scm vars"
-    String build_id = env.scmVars.BUILD_ID?: "0"
-    String git_branch = env.scmVars.GIT_BRANCH?: "Cannot find branch name"
-    String git_commit = env.scmVars.GIT_COMMIT?: "Cannot find git_commit name"
+    String build_id = env.BUILD_ID?: "0"
     String job_base_name = env.JOB_BASE_NAME ?: "Base name"
     String build_url = env.BUILD_URL ?: "Build Url"
+
+    String scmVars = env.scmVars?: "no scm vars"
+    String git_branch = env.scmVars.GIT_BRANCH?: "Cannot find branch name"
+    String git_commit = env.scmVars.GIT_COMMIT?: "Cannot find git_commit name"
+    
+    
 
     // rtp parserName: 'HTML', stableText: "<a href='http://localhost:8081/view/Test/job/REPORT/ws/RPA_RAPORT_ROBOT_217.xlsx'>RAPORT</a>"
     // rtp parserName: 'HTML', stableText: "<a href='http://localhost:8081/view/Test/job/REPORT/ws/ROBOT_DATA_INPUT_217.xlsx'>DANE WEJSCIOWE</a>"
