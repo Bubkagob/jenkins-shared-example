f61126d85b295f011c2c8b4fcecf2364181a5db9
Ivan Alexandrov <i1729van@gmail.com>
2020-03-23 11:40:33 +0300

    rtpublisher

--- vars/rtpublish.groovy
+++ vars/rtpublish.groovy
@@ -1,5 +1,44 @@
 def call(currentBuild){
-    String stableText = "Hello"
+    currentBuild.result = "SUCCESS"
+    rtp parserName: 'HTML', stableText: "<a href='http://localhost:8081/view/Test/job/REPORT/ws/RPA_RAPORT_ROBOT_217.xlsx'>RAPORT</a>"
+    rtp parserName: 'HTML', stableText: "<a href='http://localhost:8081/view/Test/job/REPORT/ws/ROBOT_DATA_INPUT_217.xlsx'>DANE WEJSCIOWE</a>"
+
+    rtp parserName: 'HTML', stableText: "<br><br>LOGS: "
+    rtp parserName: 'HTML', stableText: currentBuild.result
+    rtp parserName: 'HTML', stableText: "${BUILD_ID}"
+    String basetRow = ""
+    String stableText = ""
+    
+    stableText += "<br><br><b>ROW 1</b>"
+    stableText += "<br><b>WINDOWS</b>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Logowanie1/Logowanie1.html'>Logowanie</a>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Weryfikacja1/Weryfikacja1.html'>Weryfikacja kontrahenta</a>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Zmiany1/Zmiany1.html'>Zmiany na koncie</a>"
+    stableText += "<br><b>LINUX</b>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Logowanie1/Logowanie1.html'>Logowanie SAP</a>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Weryfikacja1/Weryfikacja1.html'>Ksiegowanie</a>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Zmiany1/Zmiany1.html'>Zapisanie</a>"
+    
+    stableText += "<br><br><b>ROW 2</b>"
+    stableText += "<br><b>WINDOWS</b>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Logowanie1/Logowanie1.html'>Logowanie</a>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Weryfikacja1/Weryfikacja1.html'>Weryfikacja kontrahenta</a>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Zmiany1/Zmiany1.html'>Zmiany na koncie</a>"
+    stableText += "<br><b>LINUX</b>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Logowanie1/Logowanie1.html'>Logowanie SAP</a>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Weryfikacja1/Weryfikacja1.html'>Ksiegowanie</a>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Zmiany1/Zmiany1.html'>Zapisanie</a>"
+    
+    stableText += "<br><br><b>ROW 3</b>"
+    stableText += "<br><b>WINDOWS</b>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Logowanie1/Logowanie1.html'>Logowanie</a>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Weryfikacja1/Weryfikacja1.html'>Weryfikacja kontrahenta</a>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Zmiany1/Zmiany1.html'>Zmiany na koncie</a>"
+    stableText += "<br><b>LINUX</b>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Logowanie1/Logowanie1.html'>Logowanie SAP</a>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Weryfikacja1/Weryfikacja1.html'>Ksiegowanie</a>"
+    stableText += "<br><a href='http://localhost:8081/view/Test/job/REPORT/ws/Zmiany1/Zmiany1.html'>Zapisanie</a>"
+    
 
     rtp parserName: 'HTML', stableText: "${stableText}"
 }
