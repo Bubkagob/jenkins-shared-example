273a5eac8799e4815f3e7209208f19e33aabe325
Ivan Alexandrov <i1729van@gmail.com>
2020-03-24 12:20:12 +0300

    test coverage

--- vars/publishWWW.groovy
+++ vars/publishWWW.groovy
@@ -1,17 +1,17 @@
 def call(){
     stage("Publish html"){
         script {
-            if (fileExists('report.html')) {
-                publishHTML(
-                    target : [allowMissing: false,
-                    alwaysLinkToLastBuild: true,
-                    keepAll: true,
-                    reportDir: '',
-                    reportFiles: 'report.html',
-                    reportName: 'Regression Report',
-                    reportTitles: 'The Report']
-                )
-            }
+            // if (fileExists('report.html')) {
+            //     publishHTML(
+            //         target : [allowMissing: false,
+            //         alwaysLinkToLastBuild: true,
+            //         keepAll: true,
+            //         reportDir: '',
+            //         reportFiles: 'report.html',
+            //         reportName: 'Regression Report',
+            //         reportTitles: 'The Report']
+            //     )
+            // }
             publishHTML(
                 target : [
                     allowMissing: false,
