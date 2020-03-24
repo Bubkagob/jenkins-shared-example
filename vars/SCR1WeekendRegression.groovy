817de2c74e7f99c9368ce36ee2798edd69e0de38
Ivan Alexandrov <i1729van@gmail.com>
2020-03-24 15:54:15 +0300

    build user

--- vars/SCR1WeekendRegression.groovy
+++ vars/SCR1WeekendRegression.groovy
@@ -27,11 +27,26 @@ def call(currentBuild, repo, branch, mailRecipients) {
             stage('Checkout SCM') {
                 steps {
                     script {
-                        def user = env.BUILD_USER_ID
-                        echo "${BUILD_USER}"
-                        echo "${user}"
-                        scmVars = scmCheckout(repo, branch)
-                    }
+                        wrap([$class: 'BuildUser']) {
+                            echo "BUILD_USER=${BUILD_USER}"
+                            echo "BUILD_USER_FIRST_NAME=${BUILD_USER_FIRST_NAME}"
+                            echo "BUILD_USER_LAST_NAME=${BUILD_USER_LAST_NAME}"
+                            echo "BUILD_USER_ID=${BUILD_USER_ID}"
+                            echo "BUILD_USER_EMAIL=${BUILD_USER_EMAIL}"
+                            echo "---"
+                            echo "env.BUILD_USER=${env.BUILD_USER}"
+                            echo "env.BUILD_USER_FIRST_NAME=${env.BUILD_USER_FIRST_NAME}"
+                            echo "env.BUILD_USER_LAST_NAME=${env.BUILD_USER_LAST_NAME}"
+                            echo "env.BUILD_USER_ID=${env.BUILD_USER_ID}"
+                            echo "env.BUILD_USER_EMAIL=${env.BUILD_USER_EMAIL}"
+                        }
+
+                    // script {
+                    //     def user = env.BUILD_USER_ID
+                    //     echo "${BUILD_USER}"
+                    //     echo "${user}"
+                    //     scmVars = scmCheckout(repo, branch)
+                    // }
                 }
             }
 
