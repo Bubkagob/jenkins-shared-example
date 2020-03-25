06b8b84d376b808fbc2cb0fe61d51a9150d03adf
Ivan Alexandrov <i1729van@gmail.com>
2020-03-25 14:19:24 +0300

    coverage OK

--- vars/publishWWW.groovy
+++ vars/publishWWW.groovy
@@ -15,7 +15,7 @@ def call(){
             fileList = findFiles(glob: '**/*dashboard.html')
             fileList.each{
                 founded->
-                String coverageDir = founded.path.minus(founded.name)
+                coverageDir = founded.path.minus(founded.name)
                 echo "Here is Dir"
                 echo founded
             }
