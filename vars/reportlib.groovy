f08f5fd7e70b236302e131af142ec9f4f743fef3
Ivan Alexandrov <i1729van@gmail.com>
2020-03-27 17:23:33 +0300

    new ive analyzer

--- vars/reportlib.groovy
+++ vars/reportlib.groovy
@@ -83,6 +83,55 @@ def generateTextReport(build_dir){
 }
 
 @NonCPS
+def getIVEReport(build_dir){
+    // for (founded in findFiles(glob: "**/*results*.txt")) {
+    //     founded.each{
+    //         resultFile ->
+    //         println resultFile.path
+    //     }
+    // }
+    def testAlignFormat = "| %-30s|%60s\t|%n";
+    def resultString = ""
+    resultString = resultString.concat("+"+"-"*95+"+\n")
+    resultString += String.format(testAlignFormat, "", "Failed tests")
+    resultString = resultString.concat("+"+"-"*95+"+\n")
+    def passed = 0
+    def failed = 0
+
+    new File(build_dir).eachFileRecurse(FILES) {
+        if(it.name.matches("results.*")) {
+            resultString += String.format(testAlignFormat, it.name, "");
+            println it.name
+            int passed_counter = 0
+            int failed_counter = 0
+
+            it.eachLine { line ->
+                if (line.contains("PASSED")){
+                    passed_counter += 1
+                }
+                else if (line.contains("FAILED")){
+                    failed_counter += 1
+                    String result = line.substring(line.indexOf("/") + 1, line.indexOf("*"));
+                    resultString += String.format(testAlignFormat, "", result.trim())
+                }
+                
+            }
+            passed += passed_counter
+            failed += failed_counter
+            resultString += String.format(testAlignFormat, "Passed:", passed_counter);
+            resultString += String.format(testAlignFormat, "Failed:", failed_counter);
+            resultString = resultString.concat("+"+"-"*95+"+\n")
+        }
+    }
+    resultString += String.format(testAlignFormat, "Total:", passed + failed);
+    resultString += String.format(testAlignFormat, "Total Passed:", passed);
+    resultString += String.format(testAlignFormat, "Total Failed:", failed);
+    resultString = resultString.concat("+"+"-"*95+"+\n")
+    int total = passed+failed
+    return new ReportResult(failed: failed, total: total, report: resultString)
+}
+
+@NonCPS
 def generateHTMLreport(build_dir){
     def resultString = ""
     def failedReport = getFailedReport(build_dir)
---
+++ vars/tempNewIveSimulation.groovy
@@ -0,0 +1,311 @@
+#!/usr/bin/env groovy
+def call(currentBuild, String repo, String branch, String mailRecipients, String toolchain, def scenarios = null, String platformDir = null) {
+    env.TOOLCHAIN = toolchain
+    pipeline {
+        agent {
+            label "beta"
+        }
+        environment {
+            FTP_DIR = new Date().format("yy_MM_dd_${BUILD_NUMBER}", TimeZone.getTimeZone('Europe/Moscow'))
+            
+        }
+        options {
+            buildDiscarder(logRotator(numToKeepStr:'5'))
+            disableConcurrentBuilds()
+            timestamps()
+        }
+        triggers{
+            pollSCM('H/5 * * * *')
+        }
+        // parameters {
+        //     choice(
+        //         name: "toolchain",
+        //         choices: ['development', 'development_ia', 'development_is'],
+        //         description: 'Select branch'
+        //     )
+        // }
+        // stages{
+        //     stage('Checkout SCM') {
+        //         steps {
+        //             script {
+        //                 //echo scenarios.size().toString()
+        //                 scmSimpleCheckout(repo, branch)
+        //             }
+        //         }
+        //     }
+
+        //     stage('Push To VM') {
+        //         steps {
+        //             script {
+        //                 pushToVm()
+        //             }
+        //         }
+        //     }
+
+            // stage("Build tests"){
+            //     agent{
+            //         label "power"
+            //     }
+            //     steps{
+            //         script {
+            //             if(scenarios){
+            //                 scenarios.each{ scenario ->
+            //                     stage("Build ${scenario}"){
+            //                         sh """
+            //                         #!/bin/bash -l
+            //                         export RISCV=${toolchain}
+            //                         export PATH=\$RISCV/bin:\$PATH
+            //                         cd encr/ive
+            //                         cd tests_src
+            //                         chmod +x build_rtl_sim.sh
+            //                         \$(PLF_SCENARIO=${scenario} ./build_rtl_sim.sh > log_TCM.txt 2>&1)
+            //                         """
+            //                     }
+            //                 }
+            //             } else {
+            //                 sh """
+            //                     #!/bin/bash -l
+            //                     export RISCV=${toolchain}
+            //                     export PATH=\$RISCV/bin:\$PATH
+            //                     cd encr/ive
+            //                     cd tests_src
+            //                     chmod +x build_rtl_sim.sh
+            //                     ./build_rtl_sim.sh
+            //                 """
+            //             }
+            //         }
+            //     }
+            // }
+
+            // stage("Build simulator"){
+            //     agent{
+            //         label "power"
+            //     }
+            //     steps{
+            //         sh """
+            //             #!/bin/bash -l
+            //             export RISCV=${toolchain}
+            //             export PATH=\$RISCV/bin:\$PATH
+            //             cd encr/ive
+            //             cd rtl_src
+            //             make build_vcs
+            //         """
+            //         sh "ls -la"
+            //     }
+            // }
+
+            // stage('Run simulation') {
+            //     agent{
+            //         label "power"
+            //     }
+            //     steps {
+            //         script {
+                        
+            //             scenarios.each{ scenario ->
+            //                 stage("Run simulation ${scenario}"){
+            //                     sh """
+            //                     #!/bin/bash -l
+            //                     cd encr/ive
+            //                     cd rtl_src
+            //                     export RISCV=${toolchain}
+            //                     export PATH=\$RISCV/bin:\$PATH
+            //                     make PLF_SCENARIO=${scenario} run_vcs MEM=tcm
+            //                     """
+            //                 }
+            //             }
+            //         }
+            //     }
+            // }
+
+            // stage("Analyze and Collect antifacts"){
+            //     agent{
+            //         label "power"
+            //     }
+            //     steps{
+            //         echo "======== Generate failed.txt ========"
+            //         script {
+            //             if (fileExists('artifacts.zip')) {
+            //                 sh "rm artifacts.zip"
+            //             }
+            //         }
+            //         zip(
+            //             archive: true,
+            //             zipFile: 'artifacts.zip',
+            //             dir: 'build',
+            //             glob: '**/*results.txt, *failed.txt, **/*.xlsx, **/*.json, **/build.log'
+            //         )
+            //     }
+            // }
+            // stage("Download artifacts from 110VM"){
+            //     steps{
+            //         echo "========build & run drhtystone test========"
+            //         sh "scp -o StrictHostKeyChecking=no -P 64013 physdesign@192.168.1.110:/home/work/jenkins/workspace/${JOB_NAME}/artifacts.zip artifacts.zip"
+            //         sh 'ls -la'
+            //         unzip(
+            //             zipFile: 'artifacts.zip',
+            //             dir: 'build'
+            //         )
+            //         sh 'ls -la'
+            //     }
+            // }
+            stage("Analyze and Collect antifacts"){
+                agent{
+                    label "power"
+                }
+                steps{
+                    echo "======== Generate failed.txt ========"
+                    script {
+                        if (fileExists('artifacts.zip')) {
+                            sh "rm artifacts.zip"
+                        }
+                    }
+                    zip(
+                        archive: true,
+                        zipFile: 'artifacts.zip',
+                        dir: 'encr/ive',
+                        glob: '**/coverage/**, **/results*.txt, **/*results.txt, *failed.txt, **/*.xlsx, **/*.json, **/build.log'
+                    )
+                }
+            }
+            stage('Downloading') {
+                steps {
+                    script {
+                        downloadArtifacts()
+                        //publishWWW()
+                    }
+                }
+            }
+
+            stage("Generate reports"){
+                steps{
+                    echo "======== Generate reports with groovy scripts ========"
+                    script {
+                        def ts = new Date()
+                        def resultObject = reportlib.getIVEReport("${WORKSPACE}/encr/ive")
+                        env.BUILD_URL = "${BUILD_URL}"
+                        env.START_TIME = "${BUILD_TIMESTAMP}"
+                        env.BUILD_DATE = ts.format("yyyy-MM-dd", TimeZone.getTimeZone('Europe/Moscow'))
+                        env.COMPLETE_TIME = ts.format("EEE, MMMM dd, yyyy, HH:mm:ss '('zzz')'", TimeZone.getTimeZone('Europe/Moscow'))
+                        env.REPORT = resultObject["report"]
+                        env.TOTAL_TESTS = resultObject["total"]
+                        env.FAILED_TESTS = resultObject["failed"]
+                        writeFile(
+                            file: "report.txt",
+                            text: "${REPORT}" 
+                        )
+                        
+                        archiveArtifacts(
+                            artifacts: 'artifacts.zip, report.txt, report.html',
+                            fingerprint: true
+                        )
+                    }
+                }
+            }
+            // stage("Generate reports"){
+            //     steps{
+            //         echo "======== Generate reports with groovy scripts ========"
+            //         script {
+            //             def ts = new Date()
+            //             def resultObject = reportlib.generateTextReport("${WORKSPACE}/build")
+            //             env.GIT_BRANCH = scmVars.GIT_BRANCH
+            //             env.BUILD_URL = "${BUILD_URL}"
+            //             env.GIT_COMMIT = scmVars.GIT_COMMIT
+            //             env.START_TIME = "${BUILD_TIMESTAMP}"
+            //             env.BUILD_DATE = ts.format("yyyy-MM-dd", TimeZone.getTimeZone('Europe/Moscow'))
+            //             env.COMPLETE_TIME = ts.format("EEE, MMMM dd, yyyy, HH:mm:ss '('zzz')'", TimeZone.getTimeZone('Europe/Moscow'))
+            //             env.REPORT = resultObject["report"]
+            //             env.TOTAL_TESTS = resultObject["total"]
+            //             env.FAILED_TESTS = resultObject["failed"]
+            //             writeFile(
+            //                 file: "report.txt",
+            //                 text: "${REPORT}" 
+            //             )
+            //             def HTML_REPORT = reportlib.generateHTMLreport("${WORKSPACE}/build")
+            //             writeFile(
+            //                 file: "report.html",
+            //                 text: "${HTML_REPORT}" 
+            //             )
+            //             archiveArtifacts(
+            //                 artifacts: 'artifacts.zip, report.txt, report.html',
+            //                 fingerprint: true
+            //             )
+            //         }
+            //     }
+            // }
+
+            // stage("Publish html"){
+            //     steps{
+            //         echo "======== Publish HTML report ========"
+            //         script {
+            //             if (fileExists('report.html')) {
+            //                 publishHTML(
+            //                     target : [
+            //                         allowMissing: false,
+            //                         alwaysLinkToLastBuild: true,
+            //                         keepAll: true,
+            //                         reportDir: '',
+            //                         reportFiles: 'report.html',
+            //                         reportName: 'Regression Report',
+            //                         reportTitles: 'The Report'
+            //                     ]
+            //                 )
+            //             }
+            //         }
+            //     }
+            // }
+            
+            // stage("Check variables"){
+            //     steps {
+            //         echo "scmVars ${scmVars} "
+                    
+            //         script{
+            //             def paramValue = "development_ia"
+            //             echo "${branch}"
+            //             if (branch != "development_ia")
+            //             {
+            //                 build(
+            //                     job: 'new_release',
+            //                     parameters: [
+            //                         [
+            //                             $class: 'StringParameterValue',
+            //                             name: 'branch',
+            //                             value: paramValue
+            //                         ],
+            //                     ]
+            //                 )
+            //             }
+            //         } 
+            //     }
+            // }
+        }
+
+        // post{
+        //     always {
+        //         script{
+        //             notificators.notifyGeneral(currentBuild.result)
+        //         }
+        //         emailext(
+        //             attachmentsPattern: "report.txt, report.html",
+        //             attachLog: true,
+        //             compressLog: true,
+        //             body: '''${SCRIPT, template="regression.template"}''',
+        //             mimeType: 'text/html',
+        //             subject: "${currentBuild.fullDisplayName} ${currentBuild.durationString.minus(' and counting')} ${currentBuild.currentResult}",
+        //             to: "${mailRecipients}",
+        //             replyTo: "${mailRecipients}"
+        //             //recipientProviders: [[$class: 'DevelopersRecipientProvider']]
+        //             //recipientProviders: [[$class: 'CulpritsRecipientProvider'], [$class: 'RequesterRecipientProvider']],
+        //             //recipientProviders: [[$class: 'CulpritsRecipientProvider']]
+        //         )
+        //     }
+        // }
+        post{
+            always {
+                script{
+                    notificators.notifyGeneral(currentBuild.result)
+                    rtpublish()
+                }
+            }
+        }
+    }
+}
