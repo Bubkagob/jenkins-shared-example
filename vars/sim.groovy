1f670af5efad670f6829af251a432ec99c6d7cb5
Ivan Alexandrov <i1729van@gmail.com>
2020-03-30 19:52:41 +0300

    add sim

--- vars/sim.groovy
+++ vars/sim.groovy
@@ -1,6 +1,5 @@
 #!/usr/bin/env groovy
-def call() {
-    stage('Push Project to agent VM') {
-        echo "======== PUSH to 192.168.1.110 ========" 
-    }
+def call(config) {
+    println config.scenarios
+    println config.buses
 }
