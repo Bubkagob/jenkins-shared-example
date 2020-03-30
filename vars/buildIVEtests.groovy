d453e1c1868a74dc955e55a2007ae6126feae1a7
Ivan Alexandrov <i1729van@gmail.com>
2020-03-30 21:16:13 +0300

    build IVE tests func

---
+++ vars/buildIVEtests.groovy
@@ -0,0 +1,29 @@
+def call(scenarios){
+    
+    if(scenarios){
+      scenarios.each{
+        scenario ->
+        stage("Build ${scenario}"){
+          sh """
+          #!/bin/bash -l
+          export RISCV=${config.toolchain}
+          export PATH=\$RISCV/bin:\$PATH
+          cd encr/ive
+          cd tests_src
+          chmod +x build_rtl_sim.sh
+          \$(PLF_SCENARIO=${scenario} ./build_rtl_sim.sh > log_TCM.txt 2>&1)
+          """
+        }
+      }
+    } else {
+      sh """
+      #!/bin/bash -l
+      export RISCV=${config.toolchain}
+      export PATH=\$RISCV/bin:\$PATH
+      cd encr/ive
+      cd tests_src
+      chmod +x build_rtl_sim.sh
+      ./build_rtl_sim.sh
+      """
+    }
+}
