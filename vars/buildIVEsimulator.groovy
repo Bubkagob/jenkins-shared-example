e8062e66bc021aae987084b3abbdbed6b3691c48
Ivan Alexandrov <i1729van@gmail.com>
2020-03-30 22:29:24 +0300

    build simulator

--- vars/buildIVEsimulator.groovy
+++ vars/buildIVEsimulator.groovy
@@ -1,28 +1,26 @@
 def call(config){
-  cd rtl_src make build_vcs
-    cd rtl_src make BUS=axi build_vcs
-    if(config.buses){
-      config.buses.each{
-        bus ->
-        stage("Build bus ${bus}"){
-          sh """
-          #!/bin/bash -l
-          export RISCV=${config.toolchain}
-          export PATH=\$RISCV/bin:\$PATH
-          cd encr/ive
-          cd rtl_src
-          make BUS=${bus} build_vcs
-          """
-        }
-      }
-    } else {
-      sh """
+  if(config.buses){
+    config.buses.each{
+      bus ->
+      stage("Build bus ${bus}"){
+        sh """
         #!/bin/bash -l
         export RISCV=${config.toolchain}
         export PATH=\$RISCV/bin:\$PATH
         cd encr/ive
         cd rtl_src
-        make build_vcs
+        make BUS=${bus} build_vcs
         """
+      }
     }
+  } else {
+    sh """
+      #!/bin/bash -l
+      export RISCV=${config.toolchain}
+      export PATH=\$RISCV/bin:\$PATH
+      cd encr/ive
+      cd rtl_src
+      make build_vcs
+      """
+  }
 }
