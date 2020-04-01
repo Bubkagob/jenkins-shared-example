20c0498f986496cbdc012d60e9d53432870c18ab
Ivan Alexandrov <i1729van@gmail.com>
2020-04-01 09:26:35 +0300

    run parallel

--- vars/runBaseScenarios.groovy
+++ vars/runBaseScenarios.groovy
@@ -1,15 +1,29 @@
 def call(config){
   script {
-    def builds = [:]
-    for (scenario in config.scenarios) {
-      builds["${scenario}"] = {
-        stage("Run ${scenario}") {
-          sh "[ -d build ] && echo OK || mkdir -p build"
-          sh "sed -i -- 's/mode: cli/mode: coverage/g' tests/_scenarios/${scenario_name}"
-          sh "cd build; perl ../tests/common/framework/launcher/launch.pl --scenario ../tests/_scenarios/${scenario_name}"
-        }
+    config.scenarios.each{
+      scenario_name ->
+      stage("Run scenario ${scenario_name}"){
+        echo "${scenario_name}"
+        echo "========Run ${scenario_name}========"
+        sh "[ -d build ] && echo OK || mkdir -p build"
+        sh "sed -i -- 's/mode: cli/mode: coverage/g' tests/_scenarios/${scenario_name}"
+        sh "cd build; perl ../tests/common/framework/launcher/launch.pl --scenario ../tests/_scenarios/${scenario_name}"
       }
     }
-    parallel builds
   }
 }
+// def call(config){
+//   script {
+//     def builds = [:]
+//     for (scenario in config.scenarios) {
+//       builds["${scenario}"] = {
+//         stage("Run ${scenario}") {
+//           sh "[ -d build ] && echo OK || mkdir -p build"
+//           sh "sed -i -- 's/mode: cli/mode: coverage/g' tests/_scenarios/${scenario_name}"
+//           sh "cd build; perl ../tests/common/framework/launcher/launch.pl --scenario ../tests/_scenarios/${scenario_name}"
+//         }
+//       }
+//     }
+//     parallel builds
+//   }
+// }
