e18d6528a262d66a6f43c7402101996ab059139a
Ivan Alexandrov <i1729van@gmail.com>
2020-04-01 10:20:06 +0300

    run parallel

--- vars/baseRegression.groovy
+++ vars/baseRegression.groovy
@@ -33,132 +33,123 @@ def call(config) {
             )
         }
         stages{
-            stage('Checkout SCM') {
-                steps {
-                    script {
-                        scmVars = scmSimpleCheckout(repo, branch)
-                        sh "sed -i 's+https://+ssh://git@+g' .gitmodules"
-                        sh "git submodule sync"
-                        sh "git submodule update --init tests" 
-                    }
-                }
-            }
 
-            stage('Push To VM') {
+            stage('ECHO') {
                 steps {
-                    script {
-                        pushToVm()
-                    }
+                    echo "${CHOSEN_SCENARIOS}"
                 }
             }
-
-            stage('Make stages') {
-                agent{
-                    label "power"
-                }
-                steps {
-                    script {
-                        runBaseScenarios(config)
-                    }
-                }
-            }
-
-            stage("Analyze and Collect antifacts"){
-                agent{
-                    label "power"
-                }
-                steps{
-                    echo "======== Generate failed.txt ========"
-                    script {
-                        if (fileExists('artifacts.zip')) {
-                            sh "rm artifacts.zip"
-                        }
-                    }
-                    zip(
-                        archive: true,
-                        zipFile: 'artifacts.zip',
-                        dir: 'build',
-                        glob: '**/*results.txt, *failed.txt, **/*.xlsx, **/*.json, **/build.log'
-                    )
-                }
-            }
-            stage('Download From Artifacts from VM') {
-                steps {
-                    script {
-                        downloadArtifacts()
-                    }
-                }
-            }
-            stage("Generate reports"){
-                steps{
-                    echo "======== Generate reports with groovy scripts ========"
-                    script {
-                        def ts = new Date()
-                        def resultObject = reportlib.generateTextReport("${WORKSPACE}/build")
-                        env.BUILD_URL = "${BUILD_URL}"
-                        env.START_TIME = "${BUILD_TIMESTAMP}"
-                        env.BUILD_DATE = ts.format("yyyy-MM-dd", TimeZone.getTimeZone('Europe/Moscow'))
-                        env.COMPLETE_TIME = ts.format("EEE, MMMM dd, yyyy, HH:mm:ss '('zzz')'", TimeZone.getTimeZone('Europe/Moscow'))
-                        env.REPORT = resultObject["report"]
-                        env.TOTAL_TESTS = resultObject["total"]
-                        env.FAILED_TESTS = resultObject["failed"]
-                        writeFile(
-                            file: "report.txt",
-                            text: "${REPORT}" 
-                        )
-                        def HTML_REPORT = reportlib.generateHTMLreport("${WORKSPACE}/build")
-                        writeFile(
-                            file: "report.html",
-                            text: "${HTML_REPORT}" 
-                        )
-                        archiveArtifacts(
-                            artifacts: 'artifacts.zip, report.txt, report.html',
-                            fingerprint: true
-                        )
-                    }
-                }
-            }
-
-            stage('Publish html') {
-                steps {
-                    script {
-                        publishWWW()
-                    }
-                }
-            }
-            
-            // stage("Check variables"){
+            // stage('Checkout SCM') {
             //     steps {
-            //         echo "scmVars ${scmVars} "
-                    
-            //         script{
-            //             def paramValue = "development_ia"
-            //             echo "${branch}"
-            //             if (branch != "development_ia")
-            //             {
-            //                 build(
-            //                     job: 'new_release',
-            //                     parameters: [
-            //                         [
-            //                             $class: 'StringParameterValue',
-            //                             name: 'branch',
-            //                             value: paramValue
-            //                         ],
-            //                     ]
-            //                 )
+            //         script {
+            //             scmVars = scmSimpleCheckout(repo, branch)
+            //             sh "sed -i 's+https://+ssh://git@+g' .gitmodules"
+            //             sh "git submodule sync"
+            //             sh "git submodule update --init tests" 
+            //         }
+            //     }
+            // }
+
+            // stage('Push To VM') {
+            //     steps {
+            //         script {
+            //             pushToVm()
+            //         }
+            //     }
+            // }
+
+            // stage('Make stages') {
+            //     agent{
+            //         label "power"
+            //     }
+            //     steps {
+            //         script {
+            //             scenarios.each{
+            //                 scenario_name -> 
+            //                     stage("Run scenario ${scenario_name}"){
+            //                         echo "${scenario_name}"
+            //                         echo "========Run ${scenario_name}========"
+            //                         sh "[ -d build ] && echo OK || mkdir -p build"
+            //                         sh "sed -i -- 's/mode: cli/mode: coverage/g' tests/_scenarios/${scenario_name}"
+            //                         sh "cd build; perl ../tests/common/framework/launcher/launch.pl --scenario ../tests/_scenarios/${scenario_name}"
+            //                     }
             //             }
-            //         } 
+            //         }
+            //     }
+            // }
+
+            // stage("Analyze and Collect antifacts"){
+            //     agent{
+            //         label "power"
+            //     }
+            //     steps{
+            //         echo "======== Generate failed.txt ========"
+            //         script {
+            //             if (fileExists('artifacts.zip')) {
+            //                 sh "rm artifacts.zip"
+            //             }
+            //         }
+            //         zip(
+            //             archive: true,
+            //             zipFile: 'artifacts.zip',
+            //             dir: 'build',
+            //             glob: '**/*results.txt, *failed.txt, **/*.xlsx, **/*.json, **/build.log'
+            //         )
+            //     }
+            // }
+            // stage('Download From Artifacts from VM') {
+            //     steps {
+            //         script {
+            //             downloadArtifacts()
+            //         }
+            //     }
+            // }
+            // stage("Generate reports"){
+            //     steps{
+            //         echo "======== Generate reports with groovy scripts ========"
+            //         script {
+            //             def ts = new Date()
+            //             def resultObject = reportlib.generateTextReport("${WORKSPACE}/build")
+            //             env.BUILD_URL = "${BUILD_URL}"
+            //             env.START_TIME = "${BUILD_TIMESTAMP}"
+            //             env.BUILD_DATE = ts.format("yyyy-MM-dd", TimeZone.getTimeZone('Europe/Moscow'))
+            //             env.COMPLETE_TIME = ts.format("EEE, MMMM dd, yyyy, HH:mm:ss '('zzz')'", TimeZone.getTimeZone('Europe/Moscow'))
+            //             env.REPORT = resultObject["report"]
+            //             env.TOTAL_TESTS = resultObject["total"]
+            //             env.FAILED_TESTS = resultObject["failed"]
+            //             writeFile(
+            //                 file: "report.txt",
+            //                 text: "${REPORT}" 
+            //             )
+            //             def HTML_REPORT = reportlib.generateHTMLreport("${WORKSPACE}/build")
+            //             writeFile(
+            //                 file: "report.html",
+            //                 text: "${HTML_REPORT}" 
+            //             )
+            //             archiveArtifacts(
+            //                 artifacts: 'artifacts.zip, report.txt, report.html',
+            //                 fingerprint: true
+            //             )
+            //         }
+            //     }
+            // }
+
+            // stage('Publish html') {
+            //     steps {
+            //         script {
+            //             publishWWW()
+            //         }
             //     }
             // }
         }
 
-        post{
-            always {
-                script{
-                    notificators.notifyGeneral(currentBuild.result)
-                    rtpublish()
-                }
-            }
-        }
+        // post{
+        //     always {
+        //         script{
+        //             notificators.notifyGeneral(currentBuild.result)
+        //             rtpublish()
+        //         }
+        //     }
+        // }
     }
 }
