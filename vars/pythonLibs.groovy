de0896390c08d41f46f239bc6c2315465400ffda
Ivan Alexandrov <i1729van@gmail.com>
2020-04-03 13:21:28 +0300

    python resources

--- vars/pythonLibs.groovy
+++ vars/pythonLibs.groovy
@@ -1,4 +1,4 @@
-def call() {
+def testPy() {
 //   final pythonContent = libraryResource('com/mkobit/sharedlib/my_file.py')
 //   // There are definitely better ways to do this without having to write to the consumer's workspace
 //   writeFile(file: 'my_file.py', text: pythonContent)
