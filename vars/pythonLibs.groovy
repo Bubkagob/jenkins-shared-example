e98d3e0e8917406c076a2f189d7a396ea889e0ef
Ivan Alexandrov <i1729van@gmail.com>
2020-04-03 13:24:01 +0300

    python resources

--- vars/pythonLibs.groovy
+++ vars/pythonLibs.groovy
@@ -1,7 +1,7 @@
 def testPy() {
-//   final pythonContent = libraryResource('com/mkobit/sharedlib/my_file.py')
-//   // There are definitely better ways to do this without having to write to the consumer's workspace
-//   writeFile(file: 'my_file.py', text: pythonContent)
-//   sh('chmod +x my_file.py && ./my_file.py')
+    final pythonContent = libraryResource('com/example/shared/hello.py')
+    // There are definitely better ways to do this without having to write to the consumer's workspace
+    writeFile(file: 'my_file.py', text: pythonContent)
+    sh('chmod +x my_file.py && python3 my_file.py')
     sh("hostname; uname -r; ls -la")
 }
