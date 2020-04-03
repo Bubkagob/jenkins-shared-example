45e707b2a1c77f8770949cafbdb1282116966ea8
Ivan Alexandrov <i1729van@gmail.com>
2020-04-03 13:20:34 +0300

    python resources

---
+++ vars/pythonLibs.groovy
@@ -0,0 +1,7 @@
+def call() {
+//   final pythonContent = libraryResource('com/mkobit/sharedlib/my_file.py')
+//   // There are definitely better ways to do this without having to write to the consumer's workspace
+//   writeFile(file: 'my_file.py', text: pythonContent)
+//   sh('chmod +x my_file.py && ./my_file.py')
+    sh("hostname; uname -r; ls -la")
+}
