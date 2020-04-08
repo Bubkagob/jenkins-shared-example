f49da9fa23116ebe3b0e99c7715a97504b368cfd
Ivan Alexandrov <i1729van@gmail.com>
2020-04-08 12:23:49 +0300

    ive reporting

--- vars/tempSim.groovy
+++ vars/tempSim.groovy
@@ -43,73 +43,73 @@ def call(config) {
                 steps {
                     script {
                         buildIVEtests(config)
-                        buildIVEsimulator(config)
-                        runIVEsimulator(config)
+                        //buildIVEsimulator(config)
+                        //runIVEsimulator(config)
                     }
                 }
             }
 
-            stage("Analyze and Collect antifacts"){
-                agent{
-                    label "power"
-                }
-                steps{
-                    echo "======== Generate failed.txt ========"
-                    script {
-                        if (fileExists('artifacts.zip')) {
-                            sh "rm artifacts.zip"
-                        }
-                    }
-                    zip(
-                        archive: true,
-                        zipFile: 'artifacts.zip',
-                        dir: 'encr/ive',
-                        glob: '**/coverage/**, **/results*.txt, **/*results.txt, *failed.txt, **/*.xlsx, **/*.json, **/build.log'
-                    )
-                }
-            }
+            // stage("Analyze and Collect antifacts"){
+            //     agent{
+            //         label "power"
+            //     }
+            //     steps{
+            //         echo "======== Generate failed.txt ========"
+            //         script {
+            //             if (fileExists('artifacts.zip')) {
+            //                 sh "rm artifacts.zip"
+            //             }
+            //         }
+            //         zip(
+            //             archive: true,
+            //             zipFile: 'artifacts.zip',
+            //             dir: 'encr/ive',
+            //             glob: '**/coverage/**, **/results*.txt, **/*results.txt, *failed.txt, **/*.xlsx, **/*.json, **/build.log'
+            //         )
+            //     }
+            // }
 
-            stage('Downloading') {
-                steps {
-                    script {
-                        downloadArtifacts()
-                        //publishWWW()
-                    }
-                }
-            }
+            // stage('Downloading') {
+            //     steps {
+            //         script {
+            //             downloadArtifacts()
+            //             //publishWWW()
+            //         }
+            //     }
+            // }
 
-            stage("Generate reports"){
-                steps{
-                    script {
-                        def ts = new Date()
-                        def resultObject = reportlib.getIVEReport("${WORKSPACE}/encr/ive")
-                        env.BUILD_URL = "${BUILD_URL}"
-                        env.START_TIME = "${BUILD_TIMESTAMP}"
-                        env.BUILD_DATE = ts.format("yyyy-MM-dd", TimeZone.getTimeZone('Europe/Moscow'))
-                        env.COMPLETE_TIME = ts.format("EEE, MMMM dd, yyyy, HH:mm:ss '('zzz')'", TimeZone.getTimeZone('Europe/Moscow'))
-                        env.REPORT = resultObject["report"]
-                        env.TOTAL_TESTS = resultObject["total"]
-                        env.FAILED_TESTS = resultObject["failed"]
-                        writeFile(
-                            file: "report.txt",
-                            text: "${REPORT}" 
-                        )
+            // stage("Generate reports"){
+            //     steps{
+            //         script {
+            //             def ts = new Date()
+            //             def resultObject = reportlib.getIVEReport("${WORKSPACE}/encr/ive")
+            //             env.BUILD_URL = "${BUILD_URL}"
+            //             env.START_TIME = "${BUILD_TIMESTAMP}"
+            //             env.BUILD_DATE = ts.format("yyyy-MM-dd", TimeZone.getTimeZone('Europe/Moscow'))
+            //             env.COMPLETE_TIME = ts.format("EEE, MMMM dd, yyyy, HH:mm:ss '('zzz')'", TimeZone.getTimeZone('Europe/Moscow'))
+            //             env.REPORT = resultObject["report"]
+            //             env.TOTAL_TESTS = resultObject["total"]
+            //             env.FAILED_TESTS = resultObject["failed"]
+            //             writeFile(
+            //                 file: "report.txt",
+            //                 text: "${REPORT}" 
+            //             )
                         
-                        archiveArtifacts(
-                            artifacts: 'artifacts.zip, report.txt, report.html',
-                            fingerprint: true
-                        )
-                    }
-                }
-            }
+            //             archiveArtifacts(
+            //                 artifacts: 'artifacts.zip, report.txt, report.html',
+            //                 fingerprint: true
+            //             )
+            //         }
+            //     }
+            // }
         }
-        post{
-            always {
-                script{
-                    //notificators.notifyGeneral(currentBuild.result)
-                    rtpublish()
-                }
-            }
-        }
+        // post{
+        //     always {
+        //         script{
+        //             //notificators.notifyGeneral(currentBuild.result)
+        //             rtpublish()
+        //         }
+        //     }
+        // }
     }
 }
