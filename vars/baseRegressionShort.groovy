60033fdae2d421f3ee45b8dd43678030abbf2a6a
Ivan Alexandrov <ivan.alexandrov@syntacore.com>
2020-04-09 11:14:23 +0300

    regression short

--- vars/baseRegressionShort.groovy
+++ vars/baseRegressionShort.groovy
@@ -50,11 +50,13 @@ def call(config) {
                                     echo "${scenario_name}"
                                     echo "========Run ${scenario_name}========"
                                     
-                                    sh "[ -d build ] && echo OK || mkdir -p build"
-                                    sh "sed -i -- 's/mode: cli/mode: coverage/g' tests/_scenarios/${scenario_name}"
+                                    //sh "[ -d build ] && echo OK || mkdir -p build"
+                                    //sh "sed -i -- 's/mode: cli/mode: coverage/g' tests/_scenarios/${scenario_name}"
                                     sh """
                                     #!/bin/bash -l
                                     echo "Toolchain : ${config.toolchain}"
+                                    [ -d build ] && echo OK || mkdir -p build
+                                    sed -i -- "s/mode: cli/mode: coverage/g" tests/_scenarios/${scenario_name}
                                     export RISCV=${config.toolchain}
                                     export SWTOOLS_1_10=${config.toolchain}/bin
                                     export PATH=\$RISCV/bin:\$PATH
